93cecdf13999f97fb458ecb907728db0ffeb2ab4
Oliver <Spaceballs12345@icloud.com>
2018-05-14 17:24:29 -0700

    made a variable number of balls with random color, size, and speed

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics gfx;
+        List<ball> balls = new List<ball>(40);
+        Random rnd = new Random();
+
+        public Color Backcolor { get; private set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gfx = this.CreateGraphics();
+            for(int i = 0; i < 5; i++)
+            {
+                balls.Add(new ball(rnd.Next(0,ClientSize.Width), rnd.Next(0,ClientSize.Height), rnd.Next(10,50), rnd.Next(10,50), 100, 100, rnd));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gfx.Clear(BackColor);
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].draw(gfx,ClientSize);
+            }
+        }
+
+        private void addBall_Click(object sender, EventArgs e)
+        {
+            balls.Add(new ball(rnd.Next(0, ClientSize.Width), rnd.Next(0, ClientSize.Height), rnd.Next(10, 50), rnd.Next(10, 50), 100, 100, rnd));
+        }
+    }
+}
---
+++ WindowsFormsApp4/ball.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    class ball
+    {
+        public int x;
+        public int y;
+        public int speedx;
+        public int speedy;
+        public int red = 0;
+        public int green = 0;
+        public int blue = 0;
+        public float width;
+        public float height;
+        Random rnd;
+
+        public ball(int x, int y, int speedx, int speedy, float width, float height, Random rnd)
+        {
+            this.x = x;
+            this.y = y;
+            this.speedx = speedx;
+            this.speedy = speedy;
+            this.width = width;
+            this.height = height;
+            this.rnd = rnd;
+        }
+
+        public void draw(Graphics gfx, Size ClientSize)
+        {
+
+
+            x += speedx;
+            y += speedy;
+
+            red = rnd.Next(0, 255);
+            green = rnd.Next(0, 255);
+            blue = rnd.Next(0, 255);
+
+            width -= 0.25f;
+            height -= 0.25f;
+
+            gfx.FillEllipse(new SolidBrush(Color.FromArgb(red, green, blue)), x, y, width, height);
+
+            if (ClientSize.Height <= y + height)
+            {
+                speedy = -(Math.Abs(speedy));
+            }
+            else if (ClientSize.Width <= x + width)
+            {
+                speedx = -(Math.Abs(speedx));
+            }
+            else if (x < 0)
+            {
+                speedx = (Math.Abs(speedx));
+            }
+            else if (y < 0)
+            {
+                speedy = (Math.Abs(speedy));
+            }
+        }
+    }
+}
